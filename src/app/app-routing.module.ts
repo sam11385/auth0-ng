5fd20a76e1659c9a78e3f6a4ee10b092bbae96ed
sam11385 <samluedke@gmail.com>
2020-07-10 10:26:13 -0600

    add route guard for profile page. updates to layout.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {
     path: 'profile',
     component: ProfileComponent,
+    canActivate: [AuthGuard],
   },
 ];
 
